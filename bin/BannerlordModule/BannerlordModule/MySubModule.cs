0a4200d93a8f105ee2afd4f556869950232b1b3d
AJPritchard <aj_pritch@hotmail.com>
2020-04-05 14:24:47 -0700

    First C# Mod! Adds button that prints hello world

---
+++ bin/BannerlordModule/BannerlordModule/MySubModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace BannerlordModule
+{
+    public class MySubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Message",
+            new TextObject("Message", null),
+            9990, () => { InformationManager.DisplayMessage(new InformationMessage("Hello World!")); }, false));
+        }
+    }
+}
