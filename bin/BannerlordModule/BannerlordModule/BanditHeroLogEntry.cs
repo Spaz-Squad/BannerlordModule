544bd4dcd2c154917484d9b2a1339b13837e6cf5
AJPritchard <aj_pritch@hotmail.com>
2020-04-05 18:45:04 -0700

    Added two new C# Classes. Started working on them
    I added two classes, BanditPartyScanner, and BanditHeroLogEntry. They together create and initialize a new hero whenever a bandit party reaches size 30 or more.

---
+++ bin/BannerlordModule/BannerlordModule/BanditHeroLogEntry.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Helpers;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.SaveSystem;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Encyclopedia;
+
+
+
+namespace BannerlordModule
+{
+    class BanditHeroLogEntry : LogEntry, IEncyclopediaLog, IChatNotification
+    {
+
+        public bool IsVisibleNotification
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override ChatNotificationType NotificationType
+        {
+            get
+            {
+                return base.PoliticalNotification(this.banditHero.Clan);
+            }
+        }
+
+        public TextObject GetNotificationText()
+        {
+            return this.GetEncyclopediaText();
+        }
+
+        public bool IsVisibleInEncyclopediaPageOf<T>(T obj) where T : MBObjectBase
+        {
+            return obj == this.banditHero;
+        }
+
+        public BanditHeroLogEntry(Hero banditHero)
+        {
+            this.banditHero = banditHero;
+        }
+
+        public TextObject GetEncyclopediaText()
+        {
+            TextObject textObject = GameTexts.FindText("str_notification_character_born", null);
+            StringHelpers.SetCharacterProperties("HERO", this.banditHero.CharacterObject, null, textObject);
+            StringHelpers.SetCharacterProperties("MOTHER", this.banditHero.Mother.CharacterObject, null, textObject);
+            StringHelpers.SetCharacterProperties("FATHER", this.banditHero.Father.CharacterObject, null, textObject);
+            return textObject;
+        }
+
+        public override string ToString()
+        {
+            return this.GetEncyclopediaText().ToString();
+        }
+
+        [SaveableField(100)]
+        public readonly Hero banditHero;
+    }
+}
---
+++ bin/BannerlordModule/BannerlordModule/BanditPartyScanner.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Helpers;
+using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.CampaignSystem.Barterables;
+using TaleWorlds.CampaignSystem.CharacterDevelopment.Managers;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
+
+namespace BannerlordModule 
+{
+    public class BanditPartyScanner : CampaignBehaviorBase
+    {
+        public override void RegisterEvents()
+        {
+            CampaignEvents.HeroCreated.AddNonSerializedListener(this, new Action<Hero, bool>(this.HeroCreated));
+        }
+
+        public override void SyncData(IDataStore dataStore)
+        { }
+
+        public void DailyTick()
+        {
+            foreach (MobileParty mobileParty in MobileParty.All)
+            {
+                if (mobileParty.IsBandit && mobileParty.IsPartyTradeActive)
+                {
+                    //mobileParty.PartyTradeGold = (int)((double)mobileParty.PartyTradeGold * 0.95 + (double)(50f * (float)mobileParty.Party.MemberRoster.TotalManCount * 0.05f));
+                    if (mobileParty.Party.NumberOfAllMembers > 30 && mobileParty.MapEvent != null)
+                    {
+
+                        Hero bandit_hero = new Hero();
+                        HeroCreated(bandit_hero, false);
+                        mobileParty.Party.Owner = bandit_hero;
+                        
+                    }
+                }
+            }
+        }
+
+        /*
+         * Here it creates a bandit hero
+         */
+        private void HeroCreated(Hero hero, bool isBornNaturally)
+        {
+            LogEntry.AddLogEntry(new BanditHeroLogEntry(hero), CampaignTime.Now);
+        }
+    }
+}
