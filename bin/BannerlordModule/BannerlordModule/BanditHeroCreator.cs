3d41aeaef5cc5a3e5bbc40e7c4608ab52f4086b8
AJPritchard <aj_pritch@hotmail.com>
2020-04-07 15:37:10 -0700

    Hero Creates Okay

--- bin/BannerlordModule/BannerlordModule/BanditHeroCreator.cs
+++ bin/BannerlordModule/BannerlordModule/BanditHeroCreator.cs
@@ -21,12 +21,6 @@ namespace BannerlordModule
 
             birthDay = HeroHelper.GetRandomBirthDayForAge((float)age);
 
-            //newCharacter.HeroObject.BirthDay = birthDay;
-            //newCharacter.HeroObject.BornSettlement = SettlementHelper.FindRandomSettlement((Settlement x) => x.IsTown && x.Culture == newCharacter.Culture);
-            //newCharacter.HeroObject.StaticBodyProperties = BodyProperties.GetRandomBodyProperties(template.IsFemale, template.GetBodyPropertiesMin(false), template.GetBodyPropertiesMax(), 0, MBRandom.RandomInt(), newCharacter.HairTags, newCharacter.BeardTags, newCharacter.TattooTags).StaticProperties;
-            //newCharacter.HeroObject.DynamicBodyProperties = new DynamicBodyProperties(newCharacter.HeroObject.Age, 0f, 0f);
-            //newCharacter.Name = new TextObject("{=!}spc", null);
-
             CultureObject banditCulture = null;
 
             banditCulture = MBObjectManager.Instance.GetObjectTypeList<CultureObject>().FirstOrDefault(x => x.IsMainCulture && x.GetName().ToString() == "Vagabond");
@@ -44,22 +38,18 @@ namespace BannerlordModule
                 InformationManager.DisplayMessage(new InformationMessage("CULTURE IS NULL"));
 
             }
-            //CustomClan clan = new CustomClan("Bandit Clan", "Bandits", banditCulture);
+            CustomClan clan = new CustomClan("Bandit Clan", "Bandits", banditCulture);
             //clan.AddClan(game);
 
             //newCharacter.HeroObject.Clan = clan.getClan();
             //hero = game.ObjectManager.RegisterPresumedObject<Hero>(new Hero());
 
-            Hero hero = null;
             CharacterObject lord = (from x in CharacterObject.Templates
                                         where x.Occupation == Occupation.Wanderer
                                         select x).GetRandomElement<CharacterObject>();
             lord.Name = new TextObject("Bandit Lord Test", null);
             lord.Culture = banditCulture;
-            //Settlement randomElement = (from settlement in Settlement.All
-            //                            where settlement.Culture == lord.Culture && settlement.IsTown
-             //                           select settlement).GetRandomElement<Settlement>();
-            //Hero hero = HeroCreator.CreateSpecialHero(lord, randomElement, null, null, -1);
+            Hero hero = HeroCreator.CreateSpecialHero(lord, null, null, null, -1);
             //GiveGoldAction.ApplyBetweenCharacters(null, hero, 20000, true);
             //hero.HasMet = true;
             //hero.Clan = clan.getClan();
